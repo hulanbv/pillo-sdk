4028fcd6a181752c35491f23383ae2fe7128644c
Jeffrey Lanters <jeffreylanters@me.com>
2021-11-01 11:03:41 +0100

    Added Native Development Environment

---
+++ Development/Application.swift
@@ -0,0 +1,30 @@
+//
+//  pillo_sdk_developmentApp.swift
+//  pillo-sdk-development
+//
+//  Created by Jeffrey Lanters on 01/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct Application: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Pillo SDK!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
