f028a7af4f66f883f076e9b7ac29c999b2239afb
Jeffrey Lanters <jeffreylanters@me.com>
2021-11-04 11:20:10 +0100

    Added Post build hook

---
+++ Editor/BluetoothPostProcessBuild.cs
@@ -0,0 +1,21 @@
+﻿#if UNITY_TVOS
+using UnityEditor.Callbacks;
+using UnityEditor;
+using UnityEditor.iOS.Xcode;
+using System.IO;
+
+public class BluetoothPostProcessBuild {
+  [PostProcessBuild]
+  public static void ChangeXcodePlist (BuildTarget buildTarget, string pathToBuiltProject) {
+    if (buildTarget == BuildTarget.iOS || buildTarget == BuildTarget.tvOS) {
+      var plistPath = pathToBuiltProject + "/Info.plist";
+      var plist = new PlistDocument ();
+      plist.ReadFromString (File.ReadAllText (plistPath));
+      var rootDict = plist.root;
+      rootDict.SetString ("NSBluetoothPeripheralUsageDescription", "Uses BLE to communicate with devices.");
+      rootDict.SetString ("NSBluetoothAlwaysUsageDescription", "Uses BLE to communicate with devices.");
+      File.WriteAllText (plistPath, plist.WriteToString ());
+    }
+  }
+}
+#endif
