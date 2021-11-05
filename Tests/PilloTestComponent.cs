8c3ba55b02e5b015801b7c5506f079c5079c3dd7
Jeffrey Lanters <jeffreylanters@me.com>
2021-11-05 17:28:41 +0100

    Added Test

---
+++ Tests/PilloTestComponent.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using Hulan.Pillo.SDK;
+
+namespace Hulan.Pillo.Test {
+  public class PilloTestComponent : MonoBehaviour, PilloFramework.IPilloFrameworkDelegate {
+    public PilloFramework pilloFramework;
+
+    public void Start () {
+      this.pilloFramework = new PilloFramework (this);
+    }
+
+    public void OnDidInitialize () {
+      Debug.Log ("Pillo Test Component did initialize");
+    }
+
+    public void OnBluetoothNotAvailable () {
+      Debug.Log ("Pillo Test Component bluetooth not available");
+    }
+
+    public void OnConnectionSuccessful (string peripheralIdentifier) {
+      Debug.Log ("Pillo Test Component connection successful: " + peripheralIdentifier);
+    }
+
+    public void OnConnectionFailed (string peripheralIdentifier) {
+      Debug.Log ("Pillo Test Component connection failed: " + peripheralIdentifier);
+    }
+
+    public void OnBatteryLevelDidChange (int batteryLevel) {
+      Debug.Log ("Pillo Test Component battery level did change: " + batteryLevel);
+    }
+
+    public void OnPressureDidChange (int pressure) {
+      Debug.Log ("Pillo Test Component pressure did change: " + pressure);
+    }
+  }
+}
