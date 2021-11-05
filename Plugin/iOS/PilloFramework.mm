c398b0bb47431b19627d563ff8592fb84b40e8ff
Jeffrey Lanters <jeffreylanters@me.com>
2021-11-05 11:06:56 +0100

    Implemented Characters

--- Plugin/iOS/PilloFramework.mm
+++ Plugin/iOS/PilloFramework.mm
@@ -13,6 +13,12 @@ extern "C" {
 // Pillo Framework implementation
 @implementation PilloFramework
 
+// Definitions
+#define PILLO_SERVICE_UUID @"579BA43D-A351-463D-92C7-911EC1B54E35";
+#define PRESSURE_CHARACTERISTIC_UUID @"1470CA75-5D7E-4E16-A70D-D1476E8D0C6F";
+#define CHARGE_CHARACTERISTIC_UUID @"22FEB891-0057-4A3E-AF5B-EC769849077C";
+#define BATTERY_LEVEL_CHARACTERISTIC_UUID @"Battery Level";
+
 - (void)initialize {
   self.centralManager = [[CBCentralManager alloc] initWithDelegate:self queue:nil options:nil];
 }
@@ -53,11 +59,41 @@ extern "C" {
 // Delegate Method invoked when the Peripheral did connect.
 - (void)centralManager:(CBCentralManager *)central didConnectPeripheral:(CBPeripheral *)peripheral {
   NSLog(@"PILLO~ Connected to the %@ (%@)", peripheral.name, peripheral.identifier.UUIDString);
+  // Once the Pillo Peripheral is connected, we'll start discovering Services.
+  // We pass nil here to request all Services be discovered.
+  [peripheral discoverServices:nil];
 }
 
 // Delegate Method invoked when the Peripheral did fail to connect.
 - (void)centralManager:(CBCentralManager *)central didFailToConnectPeripheral:(CBPeripheral *)peripheral error:(NSError *)error {
   NSLog(@"PILLO~ Error while connecting %@ (%@)", peripheral.name, peripheral.identifier.UUIDString);
+}
+
+// Delegate Method invoked when a Service is discovered.
+- (void)peripheral:(CBPeripheral *)peripheral didDiscoverServices:(NSError *)error {
+  // Loop all the available Services within the Peripheral.
+  for (CBService *service in peripheral.services) {
+    // We're going to discover the Characteristics for all Services.
+    [peripheral discoverCharacteristics:nil forService:service];
+  }
+}
+
+// Delegate Method invoked when a Characteristic for a Service is discovered.
+- (void)peripheral:(CBPeripheral *)peripheral didDiscoverCharacteristicsForService:(CBService *)service error:(NSError *)error {
+  for (CBCharacteristic *characteristic in service.characteristics) {
+    // We'll loop all of the Service's Characteristics and enable Notifications.
+    [self.pilloPeripheral setNotifyValue:YES forCharacteristic:characteristic];
+  }
+}
+
+- (void)peripheral:(CBPeripheral *)peripheral didUpdateValueForCharacteristic:(CBCharacteristic *)characteristic error:(NSError *)error {
+  // Extract the data from the Characteristic's value property and convert it
+  // into raw data based on the Characteristic's UUID.
+  NSData *rawBytes = characteristic.value;
+  uint32_t rawInt = 0;
+  [rawBytes getBytes:&rawInt length:sizeof(rawInt)];
+  NSLog(@"PILLO~ Value %u %@", rawInt, characteristic.UUID);
+  // TODO check the Characteristic's UUID and send back the data accordingly.
 }
 
 // Delegate Method invoked when the Peripheral Manager's state did update.
