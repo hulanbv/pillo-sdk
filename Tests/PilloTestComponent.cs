01d1aea639a1e8f9da52e979a05aed8339dda9f8
Jeffrey Lanters <jeffreylanters@me.com>
2021-11-09 12:59:21 +0100

    Updated Example

--- Tests/PilloTestComponent.cs
+++ Tests/PilloTestComponent.cs
@@ -13,28 +13,37 @@ namespace Hulan.Pillo.Test {
       PilloFramework.onPressureDidChange += this.OnPressureDidChange;
     }
 
+    public void OnDestroy () {
+      PilloFramework.onDidInitialize -= this.OnDidInitialize;
+      PilloFramework.onDidFailToInitialize -= this.OnDidFailToInitialize;
+      PilloFramework.onPilloDidConnect -= this.OnPilloDidConnect;
+      PilloFramework.onPilloDidFailToConnect -= this.OnPilloDidFailToConnect;
+      PilloFramework.onBatteryLevelDidChange -= this.OnBatteryLevelDidChange;
+      PilloFramework.onPressureDidChange -= this.OnPressureDidChange;
+    }
+
     public void OnDidInitialize () {
-      Debug.Log ("Pillo Test Component did initialize");
+      Debug.Log ("Pillo Test Component: did initialize");
     }
 
     public void OnDidFailToInitialize (string reason) {
-      Debug.Log ("Pillo Test Component did fail to initialize");
+      Debug.Log ("Pillo Test Component: did fail to initialize");
     }
 
     public void OnPilloDidConnect (string identifier) {
-      Debug.Log ("Pillo Test Component connection successful: " + identifier);
+      Debug.Log ("Pillo Test Component: connection successful: " + identifier);
     }
 
     public void OnPilloDidFailToConnect (string identifier) {
-      Debug.Log ("Pillo Test Component connection failed: " + identifier);
+      Debug.Log ("Pillo Test Component: connection failed: " + identifier);
     }
 
     public void OnBatteryLevelDidChange (int batteryLevel) {
-      Debug.Log ("Pillo Test Component battery level did change: " + batteryLevel);
+      Debug.Log ("Pillo Test Component: battery level did change: " + batteryLevel);
     }
 
     public void OnPressureDidChange (int pressure) {
-      Debug.Log ("Pillo Test Component pressure did change: " + pressure);
+      Debug.Log ("Pillo Test Component: pressure did change: " + pressure);
     }
   }
 }
