dbf698f86a53fcae4d1d9be20e7feffc4ea5b104
Jeffrey Lanters <jeffreylanters@me.com>
2021-11-09 12:58:28 +0100

    Updated Platform Requirements

--- Editor/UnityEditorHooks.cs
+++ Editor/UnityEditorHooks.cs
@@ -7,17 +7,23 @@ using System.IO;
 namespace Hulan.Pillo.Editor {
   public static class UnityEditorHooks {
 
+    /// <summary>
+    /// When the Unity build is succesfull, this hook will be invoked. This adds
+    /// the required usage descriptions to the Info PList to the Xcode project.
+    /// This is required in order to run the Application on the target hardware.
+    /// </summary>
     [PostProcessBuild]
     public static void OnPostProcessBuild (BuildTarget buildTarget, string pathToBuiltProject) {
-      if (buildTarget == BuildTarget.iOS || buildTarget == BuildTarget.tvOS) {
-        var plistPath = pathToBuiltProject + "/Info.plist";
-        var plist = new PlistDocument ();
-        plist.ReadFromString (File.ReadAllText (plistPath));
-        var rootDict = plist.root;
-        rootDict.SetString ("NSBluetoothPeripheralUsageDescription", "Uses BLE to communicate with devices.");
-        rootDict.SetString ("NSBluetoothAlwaysUsageDescription", "Uses BLE to communicate with devices.");
-        File.WriteAllText (plistPath, plist.WriteToString ());
-      }
+      // Fetches the Info PList and parses it.
+      var plistPath = pathToBuiltProject + "/Info.plist";
+      var plist = new PlistDocument ();
+      plist.ReadFromString (File.ReadAllText (plistPath));
+      var rootDict = plist.root;
+      // Adds the required usage descriptions to the Info PList.
+      rootDict.SetString ("NSBluetoothPeripheralUsageDescription", "Uses BLE to communicate with devices.");
+      rootDict.SetString ("NSBluetoothAlwaysUsageDescription", "Uses BLE to communicate with devices.");
+      // Writes the Info PList back to the Xcode project.
+      File.WriteAllText (plistPath, plist.WriteToString ());
     }
   }
 }
