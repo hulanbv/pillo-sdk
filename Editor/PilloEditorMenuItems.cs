3ea5066f149265a2a42c47692130231dc93986e1
Jeffrey Lanters <jeffreylanters@me.com>
2021-11-17 11:36:48 +0100

    Updated documentation

--- Editor/PilloEditorMenuItems.cs
+++ Editor/PilloEditorMenuItems.cs
@@ -11,21 +11,29 @@ namespace Hulan.PilloSDK.Editor {
 
     [MenuItem ("Pillo SDK/Input System/Simulate OnPeripheralDidConnect (1)")]
     private static void SimulateOnPeripheralDidConnect1 () {
+      // Simlates a SendMessage to the Pillo Framework's Callback Listener
+      // which tells the Pillo Framework that a peripheral has been connected
       GameObject.Find ("~PilloFrameworkCallbackListener").SendMessage ("OnPeripheralDidConnect", "faux-pillo-1");
     }
 
     [MenuItem ("Pillo SDK/Input System/Simulate OnPeripheralDidConnect (2)")]
     private static void SimulateOnPeripheralDidConnect2 () {
+      // Simlates a SendMessage to the Pillo Framework's Callback Listener
+      // which tells the Pillo Framework that a peripheral has been connected
       GameObject.Find ("~PilloFrameworkCallbackListener").SendMessage ("OnPeripheralDidConnect", "faux-pillo-2");
     }
 
     [MenuItem ("Pillo SDK/Input System/Simulate OnPeripheralDidDisconnect (1)")]
     private static void SimulateOnPeripheralDidDisconnect1 () {
+      // Simlates a SendMessage to the Pillo Framework's Callback Listener
+      // which tells the Pillo Framework that a peripheral has been disconnected
       GameObject.Find ("~PilloFrameworkCallbackListener").SendMessage ("OnPeripheralDidDisconnect", "faux-pillo-1");
     }
 
     [MenuItem ("Pillo SDK/Input System/Simulate OnPeripheralDidDisconnect (2)")]
     private static void SimulateOnPeripheralDidDisconnect2 () {
+      // Simlates a SendMessage to the Pillo Framework's Callback Listener
+      // which tells the Pillo Framework that a peripheral has been disconnected
       GameObject.Find ("~PilloFrameworkCallbackListener").SendMessage ("OnPeripheralDidDisconnect", "faux-pillo-2");
     }
   }
