17c2fe84f3a82d9a39593b2059b824b4cee64f65
Jeffrey Lanters <jeffreylanters@me.com>
2022-12-09 15:14:48 +0100

    Added device meta data payloads

---
+++ Framework/Runtime/Payloads/PeripheralHardwareVersionDidChangePayload.cs
@@ -0,0 +1,21 @@
+using System;
+
+// Unity Engine Pillo SDK Framework
+// Author: Jeffrey Lanters at Hulan
+namespace Hulan.PilloSDK.Framework.Payloads {
+  /// <summary>
+  /// Payload for the OnPeripheralHardwareDidChange event.
+  /// </summary>
+  [Serializable]
+  internal class PeripheralHardwareVersionDidChangePayload {
+    /// <summary>
+    /// The identifier of the Peripheral.
+    /// </summary>
+    public string identifier;
+
+    /// <summary>
+    /// The model number of the Peripheral.
+    /// </summary>
+    public string hardwareVersion;
+  }
+}
---
+++ Framework/Runtime/Payloads/PeripheralModelNumberDidChangePayload.cs
@@ -0,0 +1,21 @@
+using System;
+
+// Unity Engine Pillo SDK Framework
+// Author: Jeffrey Lanters at Hulan
+namespace Hulan.PilloSDK.Framework.Payloads {
+  /// <summary>
+  /// Payload for the OnPeripheralModelNumberDidChange event.
+  /// </summary>
+  [Serializable]
+  internal class PeripheralModelNumberDidChangePayload {
+    /// <summary>
+    /// The identifier of the Peripheral.
+    /// </summary>
+    public string identifier;
+
+    /// <summary>
+    /// The model number of the Peripheral.
+    /// </summary>
+    public string modelNumber;
+  }
+}
