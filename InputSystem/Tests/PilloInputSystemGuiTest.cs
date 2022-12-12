3f29f5aeb1e14d4f9023fc4e9bb1c3c609e6c39c
Jeffrey Lanters <jeffreylanters@me.com>
2022-12-12 18:19:39 +0100

    Added Input System tests

---
+++ InputSystem/Tests/PilloInputSystemGuiTest.cs
@@ -0,0 +1,43 @@
+using System;
+using UnityEngine;
+
+// Unity Engine Pillo SDK Input System
+// Author: Jeffrey Lanters at Hulan
+namespace Hulan.PilloSDK.InputSystem.Tests {
+  /// <summary>
+  /// The Pillo Input system Test MonoBehaviour will be display all the incoming
+  /// Pillo Input System events in the Unity Engine GUI.
+  /// </summary>
+  [AddComponentMenu ("Hulan/Pillo SDK/Input System/Tests/Pillo Input System Gui Test")]
+  internal class PilloInputSystemGuiTest : MonoBehaviour {
+    /// <summary>
+    /// Draws the Pillo Input System Gui Test.
+    /// </summary>
+    private void OnGUI () {
+      // Setting the GUI skin to a screen relative font size.
+      GUI.skin.label.fontSize = (int)(Screen.height * 0.02f);
+      GUI.skin.button.fontSize = (int)(Screen.height * 0.02f);
+      GUI.skin.label.fixedHeight = (int)(Screen.height * 0.03f);
+      GUI.skin.button.fixedHeight = (int)(Screen.height * 0.05f);
+      // Displaying the Pillo Input System Gui Test.
+      GUILayout.Label ("Pillo Input System Gui Test");
+      for (var i = 0; i < PilloInputSystem.pilloInputDeviceCount; i++) {
+        // Displaying each of the Pillo Input Devices.
+        var pilloInputDevice = PilloInputSystem.pilloInputDevices[i];
+        GUILayout.Label ($"Pillo Input Device {i}");
+        GUILayout.Label ($" - Identifier: {pilloInputDevice.identifier}");
+        GUILayout.Label ($" - Player Index: {pilloInputDevice.playerIndex}");
+        GUILayout.Label ($" - Pressure: {pilloInputDevice.pressure}");
+        GUILayout.Label ($" - Battery Level: {pilloInputDevice.batteryLevel}");
+        GUILayout.Label ($" - Charge State: {pilloInputDevice.chargeState}");
+        // Displaying the Pillo Input Device buttons.
+        if (GUILayout.Button ("Power Off")) {
+          pilloInputDevice.PowerOff ();
+        }
+        if (GUILayout.Button ("Start Calibration")) {
+          pilloInputDevice.StartCalibration ();
+        }
+      }
+    }
+  }
+}
