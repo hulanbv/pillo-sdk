bac0003c1e12a9494c00262a2af927fd5f6ef92d
Jeffrey Lanters <jeffreylanters@me.com>
2023-09-11 16:31:31 +0200

    Added initial handshake

--- UnityFramework/Plugin/DeviceManager.mm
+++ UnityFramework/Plugin/DeviceManager.mm
@@ -18,6 +18,8 @@
 #define COMMAND_COMMAND_CHARACTERISTIC_UUID @"A9147E1F-E91F-4A02-B6E4-2869E0FE69BB"
 #define CALIBRATION_SERVICE_UUID @"7E238267-146F-461C-8615-39B358A428A5"
 #define CALIBRATION_STARTCALIBRATION_CHARACTERISTIC_UUID @"46F9AB5B-D01A-4353-9DB4-176C4F3200CF"
+#define HANDSHAKE_SERVICE_UUID @"35865C86-7B91-4834-B44A-8A66985D1375"
+#define HANDSHAKE_HANDSHAKE_CHARACTERISTIC_UUID @"45C30C15-4815-4CDF-9ED3-9CC488492F4F"
 #define SCAN_DURATION_SECONDS 2
 #define SCAN_INTERVAL_SECONDS 10
 #define MAX_SIMULTANEOUS_PERIPHERAL_CONNECTION 2
@@ -90,7 +92,8 @@
     [CBUUID UUIDWithString:PRESSURE_SERVICE_UUID],
     [CBUUID UUIDWithString:CHARGE_SERVICE_UUID],
     [CBUUID UUIDWithString:COMMAND_SERVICE_UUID],
-    [CBUUID UUIDWithString:CALIBRATION_SERVICE_UUID]
+    [CBUUID UUIDWithString:CALIBRATION_SERVICE_UUID],
+    [CBUUID UUIDWithString:HANDSHAKE_SERVICE_UUID]
   ]];
 }
 
@@ -128,6 +131,8 @@
       [peripheral discoverCharacteristics:@[[CBUUID UUIDWithString:CALIBRATION_STARTCALIBRATION_CHARACTERISTIC_UUID]] forService:service];
     } else if ([service.UUID.UUIDString isEqualToString:DEVICEINFORMATION_SERVICE_UUID]) {
       [peripheral discoverCharacteristics:@[[CBUUID UUIDWithString:DEVICEINFORMATION_MODELNUMBER_CHARACTERISTIC_UUID], [CBUUID UUIDWithString:DEVICEINFORMATION_FIRMWAREVERSION_CHARACTERISTIC_UUID], [CBUUID UUIDWithString:DEVICEINFORMATION_HARDWAREVERSION_CHARACTERISTIC_UUID]] forService:service];
+    } else if ([service.UUID.UUIDString isEqualToString:HANDSHAKE_SERVICE_UUID]) {
+      [peripheral discoverCharacteristics:@[[CBUUID UUIDWithString:HANDSHAKE_HANDSHAKE_CHARACTERISTIC_UUID]] forService:service];
     }
   }
 }
@@ -159,6 +164,12 @@
       } else if ([characteristic.UUID.UUIDString isEqualToString:DEVICEINFORMATION_FIRMWAREVERSION_CHARACTERISTIC_UUID]) {
         [peripheral readValueForCharacteristic:characteristic];
       } else if ([characteristic.UUID.UUIDString isEqualToString:DEVICEINFORMATION_HARDWAREVERSION_CHARACTERISTIC_UUID]) {
+        [peripheral readValueForCharacteristic:characteristic];
+      }
+    }
+  } else if ([serviceUUID isEqualToString:HANDSHAKE_SERVICE_UUID]) {
+    for (CBCharacteristic *characteristic in service.characteristics) {
+      if ([characteristic.UUID.UUIDString isEqualToString:HANDSHAKE_HANDSHAKE_CHARACTERISTIC_UUID]) {
         [peripheral readValueForCharacteristic:characteristic];
       }
     }
@@ -215,6 +226,12 @@
         @"identifier": peripheral.identifier.UUIDString,
         @"modelNumber": modelNumber
       }];
+    }
+  } else if ([serviceUUID isEqualToString:HANDSHAKE_SERVICE_UUID]) {
+    if ([characteristicUUID isEqualToString:HANDSHAKE_HANDSHAKE_CHARACTERISTIC_UUID]) {
+      uint64_t handshake = 0;
+      [rawData getBytes:&handshake length:sizeof(handshake)];
+      NSLog(@"~Handshake: %llu", handshake);
     }
   }
 }
