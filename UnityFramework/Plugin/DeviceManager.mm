b549c4bc53b0c943a3b15576ab6d212ca9e2a2d6
Jeffrey Lanters <jeffreylanters@me.com>
2023-10-19 17:23:59 +0200

    Resolved linker issue

--- UnityFramework/Plugin/DeviceManager.mm
+++ UnityFramework/Plugin/DeviceManager.mm
@@ -3,32 +3,32 @@
 extern "C" {
   DeviceManager* deviceManager = nil;
 
-  void DeviceManagerInstantiate(OnCentralDidInitialize onCentralDidInitialize, OnCentralDidFailToInitialize onCentralDidFailToInitialize, OnCentralDidStartScanning onCentralDidStartScanning, OnCentralDidStopScanning onCentralDidStopScanning, OnPeripheralDidConnect onPeripheralDidConnect, OnPeripheralDidDisconnect onPeripheralDidDisconnect, OnPeripheralDidFailToConnect onPeripheralDidFailToConnect, OnPeripheralBatteryLevelDidChange onPeripheralBatteryLevelDidChange, OnPeripheralPressureDidChange onPeripheralPressureDidChange, OnPeripheralChargingStateDidChange onPeripheralChargingStateDidChange, OnPeripheralFirmwareVersionDidChange onPeripheralFirmwareVersionDidChange, OnPeripheralHardwareVersionDidChange onPeripheralHardwareVersionDidChange, OnPeripheralModelNumberDidChange onPeripheralModelNumberDidChange) {
+  void Instantiate(OnCentralDidInitialize onCentralDidInitialize, OnCentralDidFailToInitialize onCentralDidFailToInitialize, OnCentralDidStartScanning onCentralDidStartScanning, OnCentralDidStopScanning onCentralDidStopScanning, OnPeripheralDidConnect onPeripheralDidConnect, OnPeripheralDidDisconnect onPeripheralDidDisconnect, OnPeripheralDidFailToConnect onPeripheralDidFailToConnect, OnPeripheralBatteryLevelDidChange onPeripheralBatteryLevelDidChange, OnPeripheralPressureDidChange onPeripheralPressureDidChange, OnPeripheralChargingStateDidChange onPeripheralChargingStateDidChange, OnPeripheralFirmwareVersionDidChange onPeripheralFirmwareVersionDidChange, OnPeripheralHardwareVersionDidChange onPeripheralHardwareVersionDidChange, OnPeripheralModelNumberDidChange onPeripheralModelNumberDidChange) {
     if (deviceManager == nil) {
       deviceManager = [DeviceManager new];
       [deviceManager initialize:onCentralDidInitialize onCentralDidFailToInitialize:onCentralDidFailToInitialize onCentralDidStartScanning:onCentralDidStartScanning onCentralDidStopScanning:onCentralDidStopScanning onPeripheralDidConnect:onPeripheralDidConnect onPeripheralDidDisconnect:onPeripheralDidDisconnect onPeripheralDidFailToConnect:onPeripheralDidFailToConnect onPeripheralBatteryLevelDidChange:onPeripheralBatteryLevelDidChange onPeripheralPressureDidChange:onPeripheralPressureDidChange onPeripheralChargingStateDidChange:onPeripheralChargingStateDidChange onPeripheralFirmwareVersionDidChange:onPeripheralFirmwareVersionDidChange onPeripheralHardwareVersionDidChange:onPeripheralHardwareVersionDidChange onPeripheralModelNumberDidChange:onPeripheralModelNumberDidChange];
     }
   }
 
-  void DeviceManagerCancelPeripheralConnection(const char* identifier) {
+  void CancelPeripheralConnection(const char* identifier) {
     if (deviceManager != nil && identifier != nil) {
       [deviceManager cancelPeripheralConnection:[NSString stringWithUTF8String:identifier]];
     }
   }
 
-  void DeviceManagerPowerOffPeripheral(const char* identifier) {
+  void PowerOffPeripheral(const char* identifier) {
     if (deviceManager != nil && identifier != nil) {
       [deviceManager powerOffPeripheral:[NSString stringWithUTF8String:identifier]];
     }
   }
 
-  void DeviceManagerForceLedOff(const char* identifier, bool enabled) {
+  void ForceLedOff(const char* identifier, bool enabled) {
     if (deviceManager != nil && identifier != nil) {
       [deviceManager forceLedOff:[NSString stringWithUTF8String:identifier] enabled:enabled];
     }
   }
 
-  void DeviceManagerStartPeripheralCalibration(const char* identifier) {
+  void StartPeripheralCalibration(const char* identifier) {
     if (deviceManager != nil && identifier != nil) {
       [deviceManager calibratePeripheral:[NSString stringWithUTF8String:identifier]];
     }
