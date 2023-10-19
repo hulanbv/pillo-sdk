90b871485cc69177e9e8f32cd847c9dc0f3a89d3
Jeffrey Lanters <jeffreylanters@me.com>
2023-10-19 17:44:38 +0200

    Docced mono invoking

--- UnityFramework/Runtime/PilloFramework.cs
+++ UnityFramework/Runtime/PilloFramework.cs
@@ -7,42 +7,97 @@ namespace Hulan.PilloSDK.Framework {
   /// The Pillo Framework class manages the Pillo Framework Native Plugin.
   /// </summary>
   public class PilloFramework {
+    /// <summary>
+    /// Mono Callback for the Central Did Initialize event.
+    /// </summary>
     [MonoPInvokeCallback(typeof(Delegates.OnCentralDidInitialize))]
     static void OnCentralDidInitialize() => onCentralDidInitialize?.Invoke();
 
+    /// <summary>
+    /// Mono Callback for the Central Did Fail To Initialize event.
+    /// </summary>
+    /// <param name="message">The error message.</param>
     [MonoPInvokeCallback(typeof(Delegates.OnCentralDidFailToInitialize))]
     static void OnCentralDidFailToInitialize(string message) => onCentralDidFailToInitialize?.Invoke(message);
 
+    /// <summary>
+    /// Mono Callback for the Central Did Start Scanning event.
+    /// </summary>
     [MonoPInvokeCallback(typeof(Delegates.OnCentralDidStartScanning))]
     static void OnCentralDidStartScanning() => onCentralDidStartScanning?.Invoke();
 
+    /// <summary>
+    /// Mono Callback for the Central Did Stop Scanning event.
+    /// </summary>
     [MonoPInvokeCallback(typeof(Delegates.OnCentralDidStopScanning))]
     static void OnCentralDidStopScanning() => onCentralDidStopScanning?.Invoke();
 
+    /// <summary>
+    /// Mono Callback for the Peripheral Did Connect event.
+    /// </summary>
+    /// <param name="identifier">The identifier of the peripheral.</param>
     [MonoPInvokeCallback(typeof(Delegates.OnPeripheralDidConnect))]
     static void OnPeripheralDidConnect(string identifier) => onPeripheralDidConnect?.Invoke(identifier);
 
+    /// <summary>
+    /// Mono Callback for the Peripheral Did Disconnect event.
+    /// </summary>
+    /// <param name="identifier">The identifier of the peripheral.</param>
     [MonoPInvokeCallback(typeof(Delegates.OnPeripheralDidDisconnect))]
     static void OnPeripheralDidDisconnect(string identifier) => onPeripheralDidDisconnect?.Invoke(identifier);
 
+    /// <summary>
+    /// Mono Callback for the Peripheral Did Fail To Connect event.
+    /// </summary>
+    /// <param name="identifier">The identifier of the peripheral.</param>
     [MonoPInvokeCallback(typeof(Delegates.OnPeripheralDidFailToConnect))]
     static void OnPeripheralDidFailToConnect(string identifier) => onPeripheralDidFailToConnect?.Invoke(identifier);
 
+    /// <summary>
+    /// Mono Callback for the Peripheral Battery Level Did Change event.
+    /// </summary>
+    /// <param name="identifier">The identifier of the peripheral.</param>
+    /// <param name="batteryLevel">The battery level of the peripheral.</param>
     [MonoPInvokeCallback(typeof(Delegates.OnPeripheralBatteryLevelDidChange))]
     static void OnPeripheralBatteryLevelDidChange(string identifier, int batteryLevel) => onPeripheralBatteryLevelDidChange?.Invoke(identifier, batteryLevel);
 
+    /// <summary>
+    /// Mono Callback for the Peripheral Pressure Did Change event.
+    /// </summary>
+    /// <param name="identifier">The identifier of the peripheral.</param>
+    /// <param name="pressure">The pressure of the peripheral.</param>
     [MonoPInvokeCallback(typeof(Delegates.OnPeripheralPressureDidChange))]
     static void OnPeripheralPressureDidChange(string identifier, int pressure) => onPeripheralPressureDidChange?.Invoke(identifier, pressure);
 
+    /// <summary>
+    /// Mono Callback for the Peripheral Charging State Did Change event.
+    /// </summary>
+    /// <param name="identifier">The identifier of the peripheral.</param>
+    /// <param name="chargingState">The charging state of the peripheral.</param>
     [MonoPInvokeCallback(typeof(Delegates.OnPeripheralChargingStateDidChange))]
     static void OnPeripheralChargingStateDidChange(string identifier, ChargingState chargingState) => onPeripheralChargingStateDidChange?.Invoke(identifier, chargingState);
 
+    /// <summary>
+    /// Mono Callback for the Peripheral Firmware Version Did Change event.
+    /// </summary>
+    /// <param name="identifier">The identifier of the peripheral.</param>
+    /// <param name="firmwareVersion">The firmware version of the peripheral.</param>
     [MonoPInvokeCallback(typeof(Delegates.OnPeripheralFirmwareVersionDidChange))]
     static void OnPeripheralFirmwareVersionDidChange(string identifier, string firmwareVersion) => onPeripheralFirmwareVersionDidChange?.Invoke(identifier, firmwareVersion);
 
+    /// <summary>
+    /// Mono Callback for the Peripheral Hardware Version Did Change event.
+    /// </summary>
+    /// <param name="identifier">The identifier of the peripheral.</param>
+    /// <param name="hardwareVersion">The hardware version of the peripheral.</param>
     [MonoPInvokeCallback(typeof(Delegates.OnPeripheralHardwareVersionDidChange))]
     static void OnPeripheralHardwareVersionDidChange(string identifier, string hardwareVersion) => onPeripheralHardwareVersionDidChange?.Invoke(identifier, hardwareVersion);
 
+    /// <summary>
+    /// Mono Callback for the Peripheral Model Number Did Change event.
+    /// </summary>
+    /// <param name="identifier">The identifier of the peripheral.</param>
+    /// <param name="modelNumber">The model number of the peripheral.</param>
     [MonoPInvokeCallback(typeof(Delegates.OnPeripheralModelNumberDidChange))]
     static void OnPeripheralModelNumberDidChange(string identifier, string modelNumber) => onPeripheralModelNumberDidChange?.Invoke(identifier, modelNumber);
 
@@ -143,8 +198,8 @@ namespace Hulan.PilloSDK.Framework {
     /// </summary>
     /// <param name="identifier">The identifier of the peripheral.</param>
     /// <param name="enabled">Defines whether the LED should be forced off.</param>
-    public static void ForceLedOff(string identifier, bool enabled) {
-      DeviceManager.ForceLedOff(identifier, enabled);
+    public static void ForcePeripheralLedOff(string identifier, bool enabled) {
+      DeviceManager.ForcePeripheralLedOff(identifier, enabled);
     }
 
     /// <summary>
