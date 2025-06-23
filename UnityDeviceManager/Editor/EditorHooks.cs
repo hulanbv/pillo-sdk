9fb3918dd43af441ae51ff6f39c954a3e51829f5
Jeffrey Lanters <jeffreylanters@me.com>
2025-06-23 11:56:37 +0200

    Enhance documentation for EditorHooks class to clarify play mode state change handling

--- UnityDeviceManager/Editor/EditorHooks.cs
+++ UnityDeviceManager/Editor/EditorHooks.cs
@@ -6,11 +6,19 @@ namespace Hulan.PilloSDK.DeviceManager.Editor {
   /// in the Unity Editor.
   /// </summary>
   public static class EditorHooks {
+    /// <summary>
+    /// Registers a callback for the Unity Editor's play mode state changes.
+    /// </summary>
     [UnityEditor.InitializeOnLoadMethod]
     private static void RegisterPlayModeStateChangedCallback() {
       UnityEditor.EditorApplication.playModeStateChanged += OnPlayModeStateChanged;
     }
 
+    /// <summary>
+    /// Stops the Device Manager service when the Unity Editor exits play mode.
+    /// This is necessary to ensure that the service does not continue running
+    /// in the background after exiting play mode.
+    /// </summary>
     private static void OnPlayModeStateChanged(UnityEditor.PlayModeStateChange state) {
       if (state == UnityEditor.PlayModeStateChange.ExitingPlayMode) {
         PluginBridge.StopService();
