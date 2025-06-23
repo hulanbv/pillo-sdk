d8a1dba198cd87b9fd9a5832780f84e8032d88a1
Jeffrey Lanters <jeffreylanters@me.com>
2025-06-23 13:33:27 +0200

    Refactor EditorHooks to improve play mode state change handling and service management

--- UnityDeviceManager/Editor/EditorHooks.cs
+++ UnityDeviceManager/Editor/EditorHooks.cs
@@ -1,17 +1,23 @@
+using System.Diagnostics;
 using Hulan.PilloSDK.DeviceManager.Core;
+using UnityEditor;
+using UnityEditor.Compilation;
 
 namespace Hulan.PilloSDK.DeviceManager.Editor {
   /// <summary>
   /// The Editor Hooks class is used to manage the Device Manager Native Plugin
   /// in the Unity Editor.
   /// </summary>
-  public static class EditorHooks {
+  [InitializeOnLoad]
+  internal static class EditorHooks {
     /// <summary>
     /// Registers a callback for the Unity Editor's play mode state changes.
     /// </summary>
-    [UnityEditor.InitializeOnLoadMethod]
-    private static void RegisterPlayModeStateChangedCallback() {
-      UnityEditor.EditorApplication.playModeStateChanged += OnPlayModeStateChanged;
+    static EditorHooks() {
+      UnityEngine.Debug.Log("EditorHooks: Initializing Device Manager service...");
+      EditorApplication.playModeStateChanged += OnPlayModeStateChanged;
+      AssemblyReloadEvents.beforeAssemblyReload += OnBeforeAssemblyReload;
+      // EditorApplication.quitting += OnEditorQuitting;
     }
 
     /// <summary>
@@ -19,10 +25,21 @@ namespace Hulan.PilloSDK.DeviceManager.Editor {
     /// This is necessary to ensure that the service does not continue running
     /// in the background after exiting play mode.
     /// </summary>
-    private static void OnPlayModeStateChanged(UnityEditor.PlayModeStateChange state) {
-      if (state == UnityEditor.PlayModeStateChange.ExitingPlayMode) {
+    private static void OnPlayModeStateChanged(PlayModeStateChange state) {
+      if (state == PlayModeStateChange.ExitingPlayMode) {
         PluginBridge.StopService();
+        UnityEngine.Debug.Log("EditorHooks: Stopping Device Manager service on exiting play mode.");
       }
+    }
+
+    private static void OnBeforeAssemblyReload() {
+      PluginBridge.StopService();
+      UnityEngine.Debug.Log("EditorHooks: Stopping Device Manager service before assembly reload.");
+    }
+
+    private static void OnEditorQuitting() {
+      PluginBridge.StopService();
+      UnityEngine.Debug.Log("EditorHooks: Stopping Device Manager service on editor quitting.");
     }
   }
 }
