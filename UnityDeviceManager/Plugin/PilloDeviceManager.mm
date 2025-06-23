b89e1d5c2aeb809712b7d4d3e9b2b5deff9d19c7
Jeffrey Lanters <jeffreylanters@me.com>
2025-06-23 11:42:18 +0200

    Refactor PilloDeviceManager.mm to rename DeviceManager to PilloDeviceManager and update export macros for symbol visibility

--- UnityDeviceManager/Plugin/PilloDeviceManager.mm
+++ UnityDeviceManager/Plugin/PilloDeviceManager.mm
@@ -1,54 +1,69 @@
+//
+//  PilloDeviceManager.mm
+//  Pillo SDK Device Manager
+//  Plugin
+//
+//  Created by Jeffrey Lanters on 14/08/2024.
+//
+
 #import "PilloDeviceManager.h"
 
-#define EXPORT_SYMBOL extern "C" __attribute__((visibility("default")))
+#define PILLO_EXPORT_SYMBOL extern "C" __attribute__((visibility("default")))
 
-DeviceManager *deviceManager = [[DeviceManager alloc] init];
+PilloDeviceManager *pilloDeviceManager = [[PilloDeviceManager alloc] init];
 
-EXPORT_SYMBOL void SetDelegates(OnCentralDidInitialize onCentralDidInitialize, OnCentralDidFailToInitialize onCentralDidFailToInitialize, OnCentralDidStartScanning onCentralDidStartScanning, OnCentralDidStopScanning onCentralDidStopScanning, OnPeripheralDidConnect onPeripheralDidConnect, OnPeripheralDidDisconnect onPeripheralDidDisconnect, OnPeripheralDidFailToConnect onPeripheralDidFailToConnect, OnPeripheralBatteryLevelDidChange onPeripheralBatteryLevelDidChange, OnPeripheralPressureDidChange onPeripheralPressureDidChange, OnPeripheralChargingStateDidChange onPeripheralChargingStateDidChange, OnPeripheralFirmwareVersionDidChange onPeripheralFirmwareVersionDidChange, OnPeripheralHardwareVersionDidChange onPeripheralHardwareVersionDidChange, OnPeripheralModelNumberDidChange onPeripheralModelNumberDidChange) {
-  deviceManager->onCentralDidInitialize = onCentralDidInitialize;
-  deviceManager->onCentralDidFailToInitialize = onCentralDidFailToInitialize;
-  deviceManager->onCentralDidStartScanning = onCentralDidStartScanning;
-  deviceManager->onCentralDidStopScanning = onCentralDidStopScanning;
-  deviceManager->onPeripheralDidConnect = onPeripheralDidConnect;
-  deviceManager->onPeripheralDidDisconnect = onPeripheralDidDisconnect;
-  deviceManager->onPeripheralDidFailToConnect = onPeripheralDidFailToConnect;
-  deviceManager->onPeripheralBatteryLevelDidChange = onPeripheralBatteryLevelDidChange;
-  deviceManager->onPeripheralPressureDidChange = onPeripheralPressureDidChange;
-  deviceManager->onPeripheralChargingStateDidChange = onPeripheralChargingStateDidChange;
-  deviceManager->onPeripheralFirmwareVersionDidChange = onPeripheralFirmwareVersionDidChange;
-  deviceManager->onPeripheralHardwareVersionDidChange = onPeripheralHardwareVersionDidChange;
-  deviceManager->onPeripheralModelNumberDidChange = onPeripheralModelNumberDidChange;
-  [deviceManager didFinalize];
+PILLO_EXPORT_SYMBOL void PilloDeviceManagerSetDelegates(OnCentralDidInitialize onCentralDidInitialize, OnCentralDidFailToInitialize onCentralDidFailToInitialize, OnCentralDidStartScanning onCentralDidStartScanning, OnCentralDidStopScanning onCentralDidStopScanning, OnPeripheralDidConnect onPeripheralDidConnect, OnPeripheralDidDisconnect onPeripheralDidDisconnect, OnPeripheralDidFailToConnect onPeripheralDidFailToConnect, OnPeripheralBatteryLevelDidChange onPeripheralBatteryLevelDidChange, OnPeripheralPressureDidChange onPeripheralPressureDidChange, OnPeripheralChargingStateDidChange onPeripheralChargingStateDidChange, OnPeripheralFirmwareVersionDidChange onPeripheralFirmwareVersionDidChange, OnPeripheralHardwareVersionDidChange onPeripheralHardwareVersionDidChange, OnPeripheralModelNumberDidChange onPeripheralModelNumberDidChange) {
+  pilloDeviceManager->onCentralDidInitialize = onCentralDidInitialize;
+  pilloDeviceManager->onCentralDidFailToInitialize = onCentralDidFailToInitialize;
+  pilloDeviceManager->onCentralDidStartScanning = onCentralDidStartScanning;
+  pilloDeviceManager->onCentralDidStopScanning = onCentralDidStopScanning;
+  pilloDeviceManager->onPeripheralDidConnect = onPeripheralDidConnect;
+  pilloDeviceManager->onPeripheralDidDisconnect = onPeripheralDidDisconnect;
+  pilloDeviceManager->onPeripheralDidFailToConnect = onPeripheralDidFailToConnect;
+  pilloDeviceManager->onPeripheralBatteryLevelDidChange = onPeripheralBatteryLevelDidChange;
+  pilloDeviceManager->onPeripheralPressureDidChange = onPeripheralPressureDidChange;
+  pilloDeviceManager->onPeripheralChargingStateDidChange = onPeripheralChargingStateDidChange;
+  pilloDeviceManager->onPeripheralFirmwareVersionDidChange = onPeripheralFirmwareVersionDidChange;
+  pilloDeviceManager->onPeripheralHardwareVersionDidChange = onPeripheralHardwareVersionDidChange;
+  pilloDeviceManager->onPeripheralModelNumberDidChange = onPeripheralModelNumberDidChange;
 }
 
-EXPORT_SYMBOL void CancelPeripheralConnection(const char* identifier) {
-  if (deviceManager == nil || identifier == nil) {
+PILLO_EXPORT_SYMBOL void PilloDeviceManagerStartService() {
+  if (pilloDeviceManager == nil) {
     return;
   }
   
-  [deviceManager cancelPeripheralConnection:[NSString stringWithUTF8String:identifier]];
+  [pilloDeviceManager startService];
 }
 
-EXPORT_SYMBOL void PowerOffPeripheral(const char* identifier) {
-  if (deviceManager == nil || identifier == nil) {
+PILLO_EXPORT_SYMBOL void PilloDeviceManagerCancelPeripheralConnection(const char* identifier) {
+  if (pilloDeviceManager == nil || identifier == nil) {
     return;
   }
   
-  [deviceManager powerOffPeripheral:[NSString stringWithUTF8String:identifier]];
+  [pilloDeviceManager cancelPeripheralConnection:[NSString stringWithUTF8String:identifier]];
 }
 
-EXPORT_SYMBOL void ForcePeripheralLedOff(const char* identifier, bool enabled) {
-  if (deviceManager == nil || identifier == nil) {
+PILLO_EXPORT_SYMBOL void PilloDeviceManagerPowerOffPeripheral(const char* identifier) {
+  if (pilloDeviceManager == nil || identifier == nil) {
     return;
   }
   
-  [deviceManager forcePeripheralLedOff:[NSString stringWithUTF8String:identifier] enabled:enabled];
+  [pilloDeviceManager powerOffPeripheral:[NSString stringWithUTF8String:identifier]];
 }
 
-EXPORT_SYMBOL void StartPeripheralCalibration(const char* identifier) {
-  if (deviceManager == nil || identifier == nil) {
+PILLO_EXPORT_SYMBOL void PilloDeviceManagerForcePeripheralLedOff(const char* identifier, bool enabled) {
+  if (pilloDeviceManager == nil || identifier == nil) {
     return;
   }
   
-  [deviceManager calibratePeripheral:[NSString stringWithUTF8String:identifier]];
+  [pilloDeviceManager forcePeripheralLedOff:[NSString stringWithUTF8String:identifier] enabled:enabled];
+}
+
+PILLO_EXPORT_SYMBOL void PilloDeviceManagerStartPeripheralCalibration(const char* identifier) {
+  if (pilloDeviceManager == nil || identifier == nil) {
+    return;
+  }
+  
+  [pilloDeviceManager calibratePeripheral:[NSString stringWithUTF8String:identifier]];
 }
