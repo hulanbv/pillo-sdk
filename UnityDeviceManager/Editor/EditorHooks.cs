aafa563b36075f5bd4089b0600fc745a2ecf0ed9
Jeffrey Lanters <jeffreylanters@me.com>
2025-06-24 10:56:23 +0200

    Merged build tools into the device manager package

--- UnityDeviceManager/Editor/EditorHooks.cs
+++ UnityDeviceManager/Editor/EditorHooks.cs
@@ -1,6 +1,13 @@
 #if UNITY_EDITOR
 using Hulan.PilloSDK.DeviceManager.Core;
 using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEngine;
+using System.IO;
+using System;
+#if UNITY_IOS || UNITY_TVOS
+using UnityEditor.iOS.Xcode;
+#endif
 
 namespace Hulan.PilloSDK.DeviceManager.Editor {
   /// <summary>
@@ -25,6 +32,30 @@ namespace Hulan.PilloSDK.DeviceManager.Editor {
       if (state == PlayModeStateChange.ExitingPlayMode) {
         PluginBridge.StopService();
       }
+    }
+
+    /// <summary>
+    /// When the Unity build is succesfull, this hook will be invoked. This adds
+    /// the required usage descriptions to the Info Property List in the Xcode project.
+    /// This is required in order to run the Application on the target hardware.
+    /// The usage descriptions are required for Bluetooth connectivity.
+    /// </summary>
+    [PostProcessBuild]
+    internal static void OnPostProcessBuild(BuildTarget buildTarget, string pathToBuiltProject) {
+#if UNITY_IOS || UNITY_TVOS
+      try {
+        var plistPath = pathToBuiltProject + "/Info.plist";
+        var plist = new PlistDocument();
+        plist.ReadFromString(File.ReadAllText(plistPath));
+        plist.root.SetString("NSBluetoothPeripheralUsageDescription", "Allow this app to connect to a Pillo.");
+        plist.root.SetString("NSBluetoothAlwaysUsageDescription", "Allow this app to connect to a Pillo.");
+        File.WriteAllText(plistPath, plist.WriteToString());
+        Debug.Log("Pillo SDK added usage descriptions to Info.plist");
+      }
+      catch (Exception exception) {
+        Debug.LogError($"Pillo SDK failed to add usage descriptions to Info.plist: {exception.Message}");
+      }
+#endif
     }
   }
 }
